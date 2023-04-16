43640c12c439da28e6074d11235a195af5425571
LanceLun <onlyu1213@gmail.com>
2023-04-16 12:13:30 +0800

    繼承-薪水計算，第二版

---
+++ 該讀書喇同學-薪水計算/PersonClass/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 該讀書喇同學_薪水計算
+{
+	
+	internal class Employee:People
+	{
+		/// <summary>
+		/// 要有id才是員工，並且加入職位區分薪水(限定職位輸入值)
+		/// </summary>
+		public string Id { get; private set; }
+		public string position { get; private set; }
+		private int BaseSalary;
+		public Employee(string name, bool gender, string id, string position, int baseSalary)
+			: base(name, gender)
+		{
+			if (string.IsNullOrEmpty(id))
+			{
+				throw new ArgumentException("員工編號不能留空");
+			}
+			if (baseSalary < 23000)
+			{
+				throw new Exception("基本薪資不得低於勞基法");
+			}
+			if (position.ToLower() == "sales" || position.ToLower() == "engineer" || position.ToLower() == "manager") 
+			{
+				this.position = position.ToLower();
+			}
+			else { this.position = "employee"; }
+			this.Id = id;
+			this.BaseSalary = baseSalary;
+		}
+		public virtual int GetSalary() => BaseSalary;
+	}
+}
---
+++ 該讀書喇同學-薪水計算/PersonClass/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 該讀書喇同學_薪水計算
+{
+	/// <summary>
+	/// 人的基本資訊，姓名、性別
+	/// </summary>
+	internal class People
+	{
+		public string Name { get; private set; }
+		public bool Gender { get; private set; }
+		public People(string name, bool gender)
+		{
+			if (string.IsNullOrEmpty(name)) { throw new ArgumentNullException("姓名不得留空"); }
+			this.Name = name;
+			this.Gender = gender;
+		}
+	}
+}
