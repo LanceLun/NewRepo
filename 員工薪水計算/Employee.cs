95d6287e430d4d8595beaac8035846c4ab8a8da8
LanceLun <onlyu1213@gmail.com>
2023-04-19 21:53:57 +0800

    第三版

---
+++ 員工薪水計算/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 員工薪水計算
+{
+	internal abstract class Employee : Person
+	{
+		/// <summary>
+		/// 要有id才是員工，並且加入職位區分薪水(限定職位輸入值)
+		/// </summary>
+		public string Id { get; private set; }
+		public string Position { get; private set; }
+		public int BaseSalary { get; private set; }
+		public Employee(string name, bool gender, string id, string position, int baseSalary)
+			: base(name, gender)
+		{
+			if (string.IsNullOrEmpty(id))
+			{
+				throw new ArgumentException("員工編號不能留空");
+			}
+			if (baseSalary < 23000)
+			{
+				throw new Exception("基本薪資不得低於勞基法");
+			}
+			if (position.ToLower() == "sales" || position.ToLower() == "engineer" || position.ToLower() == "manager")
+			{
+				this.Position = position.ToLower();
+			}
+			else { this.Position = "employee"; }
+			this.Id = id;
+			this.BaseSalary = baseSalary;
+		}
+		public abstract int GetSalary(int baseSalary);
+	}
+}
---
+++ 員工薪水計算/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 員工薪水計算
+{
+	internal class Person
+	{
+		public string Name { get; private set; }
+		public bool Gender { get; private set; }
+		public Person(string name, bool gender)
+		{
+			if (string.IsNullOrEmpty(name)) { throw new ArgumentNullException("姓名不得留空"); }
+			this.Name = name;
+			this.Gender = gender;
+		}
+	}
+}
