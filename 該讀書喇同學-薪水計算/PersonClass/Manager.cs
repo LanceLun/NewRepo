0734943b4d812f23fef6527516571791e18923b4
LanceLun <onlyu1213@gmail.com>
2023-04-19 22:06:22 +0800

    test

---
+++ 該讀書喇同學-薪水計算/PersonClass/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 該讀書喇同學_薪水計算
+{
+	internal class Manager:Employee
+	{
+		/// <summary>
+		/// 經理領領導加給，視下屬有幾人而定，領導一人就加10W
+		/// </summary>
+		private Employee[] Subordinates;
+		public Manager(string name, bool gender, string id, string position, int baseSalary, Employee[] subordinates)
+			: base(name, gender, id, position, baseSalary)
+		{
+			this.Subordinates = subordinates;
+		}
+		public override int GetSalary(int baseSalary)
+		{
+			int leaderBounus = 0;
+			leaderBounus = Subordinates.Count() * 100000;
+			return  baseSalary + leaderBounus;
+		}
+	}
+}
